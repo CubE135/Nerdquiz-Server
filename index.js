426a367e0a0d63d34d1fb05bdbb63e0c50d948dd
CubE135 <gitlab@cub3mail.de>
2022-10-01 17:10:19 +0200

    ✨ Added player join & disconnect

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const io = new Server(server, {
 	}
 })
 
+let boards = []
+
 io.on('connection', (socket) => {
 	socket.on('create-room', (code) => {
 		if (socket.code) {
@@ -19,20 +21,56 @@ io.on('connection', (socket) => {
 		socket.join(code)
 		socket.code = code
 		socket.isHost = true
+		if (!boards[code]) {
+			boards[code] = { board: null, status: false, players: [] }
+		}
 	})
-	socket.on('join-room', (code) => {
+	socket.on('join-room', (code, name) => {
 		if (socket.code) {
 			socket.leave(socket.code)
 			delete socket.code
 			delete socket.isHost
 		}
 		if (io.sockets.adapter.rooms.get(code)) {
-			socket.join(code)
-			socket.code = code
-			socket.isHost = false
-			socket.emit('room-joined', code)
+			let player = playerIsInRoom(code, name)
+			if (player) {
+				socket.emit('name-taken')
+			}else {
+				socket.join(code)
+				socket.code = code
+				socket.isHost = false
+				socket.name = name
+				boards[code].players.push({ name: name, points: 0 })
+				socket.emit('room-joined', code, boards[code])
+				io.to(socket.code).emit('player-joined', boards[socket.code].players)
+				io.to(socket.code).emit('update-board', boards[socket.code])
+			}
 		} else {
 			socket.emit('room-not-found')
+		}
+	})
+	socket.on('update-board', (board, boardStatus) => {
+		socket.board = board
+		boards[socket.code].board = board
+		boards[socket.code].status = boardStatus
+		console.log(boards[socket.code])
+		io.to(socket.code).emit('update-board', boards[socket.code])
+	})
+	socket.on('disconnect', () => {
+		if (boards[socket.code]) {
+			if (socket.isHost) {
+				delete boards[socket.code]
+				io.to(socket.code).emit('host-left', socket.name)
+			}else {
+				console.log(boards[socket.code].players)
+				let player = playerIsInRoom(socket.code, socket.name)
+				if (player) {
+					removePlayerFromRoom(socket.code, socket.name)
+				}
+				console.log(boards[socket.code].players)
+				io.to(socket.code).emit('player-left', boards[socket.code].players)
+				io.to(socket.code).emit('update-board', boards[socket.code])
+			}
 		}
 	})
 })
@@ -40,3 +78,16 @@ io.on('connection', (socket) => {
 server.listen(3001, () => {
 	console.log('listening on *:3001')
 })
+
+function playerIsInRoom(code, name) {
+	let player = boards[code].players.find((player) => {
+		return player.name === name
+	})
+	return player
+}
+
+function removePlayerFromRoom(code, name) {
+	boards[code].players = boards[code].players.filter((player) => {
+		return player.name !== name
+	})
+}
